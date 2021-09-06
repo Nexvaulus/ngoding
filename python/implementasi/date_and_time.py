242d31a02a8be97db72bc1ca11e6b58223158885
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-06 18:37:05 +0700

    yes

--- python/implementasi/date_and_time.py
+++ python/implementasi/date_and_time.py
@@ -2,16 +2,18 @@
 
 import datetime as dt
 
-print("""
-Silahkan masukkan 
-tanggal :
-bulan : 
-tahun : 
-""")
-
 tanggal = int(input("Tanggal \t: "))
 bulan = int(input("Bulan \t\t: "))
 tahun = int(input("Tahun \t\t: "))
 
 tanggal_lahir = dt.date(tahun, bulan, tanggal)
-print(tanggal_lahir)
+print(f"Tanggal lahir anda adalah : {tanggal_lahir}")
+print(f"Hari nya adalah : {tanggal_lahir:%A}")
+
+hari_ini = dt.date.today()
+print(f"Hari ini tanggal : {hari_ini}")
+umur_hari = hari_ini - tanggal_lahir
+umur_tahun = umur_hari.days // 365
+print(umur_tahun)
+print(f"Umur anda adalah : {umur_hari.days} hari {umur_tahun} tahun")
+
