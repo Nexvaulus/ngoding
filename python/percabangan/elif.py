eef60a4bc380dcb33c084d8c3d8dd20178e79863
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-08 07:21:25 +0700

    yes

---
+++ python/percabangan/elif.py
@@ -0,0 +1,18 @@
+# ELIF = else if statement
+
+nama = input("Nama kamu siapa? ")
+
+if nama == "ucup": # kondisi 1
+    print("Hai ucup") # aksi true 1
+
+elif nama == "otong": # kondisi 2
+    print("Hai otong") # aksi true 2
+
+elif nama == "mamat": # kondisi 3
+    print("Hai mamat") # aksi true 3
+    
+else:
+    print("au ah gak kenal") # aksi false
+
+
+print("Terima kasih")
