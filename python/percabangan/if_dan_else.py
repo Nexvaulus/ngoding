5e40b5ca2a060e898f55e942083071edf85e1390
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-08 07:21:44 +0700

    masuk

---
+++ python/percabangan/if_dan_else.py
@@ -0,0 +1,35 @@
+# if dan else statement
+
+# 1. if nya
+# 2. kondisi nya
+# 3. aksi nya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+
+if nama=="ucup" : print('hai halo')
+print(f"Terima Kasih {nama}")
+
+
+# 2. program if indentation
+
+if nama=="ucup":
+        print("hai ucup")
+        print("kamu ganteng abieezz")
+print(f"Terima kasih {nama}")
+
+
+# 3. else statement
+
+if nama=="otong":
+        print("hai otong, si keren")
+else: 
+        print("ah kamu bukan otong, kamu gak keren")
+print("Terima kasih")
+
+
