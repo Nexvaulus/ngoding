4f6e444fbc7c9156a3a7c781c6b7df0dad495041
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-08 12:22:36 +0700

    percabangan

---
+++ python/percabangan/latihan_kalkulator.py
@@ -0,0 +1,30 @@
+# latihan
+# kalkulator sederhana
+
+print(20*"=")
+print("Kalkulator sederhana")
+print(20*"=" + "\n")
+
+angka_1 = float(input("Masukkan angka 1 = "))
+operator = input("Masukkan operator (+, -, x, /) : ")
+angka_2 = float(input("Masukkan angka 2 = "))
+
+# percabangan
+
+if operator == "+":
+    hasil = angka_1 + angka_2
+    print(f"hasilnya adalah {hasil}")
+elif operator == "-":
+    hasil = angka_1 - angka_2
+    print(f"hasilnya adalah {hasil}")
+elif operator == "x" or operator == "*":
+    hasil = angka_1 * angka_2
+    print(f"hasilnya adalah {hasil}")
+elif operator == "/":
+    hasil = angka_1 / angka_2
+    print(f"hasilnya adalah {hasil}")
+else:
+    print("Invalid")
+
+print("Akhir dari program, terimakasih")
+
