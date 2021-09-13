a6508bd385b7c0ce7c3dea44001f9911c7cf57f5
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-13 13:32:19 +0700

    yes

---
+++ python/perulangan/for.py
@@ -0,0 +1,31 @@
+# perulangan (loop)
+
+# for kondisi:
+#     aksi
+
+# ini dengan list
+angka_1 = [0,1,2,3,4] # ini adalah list
+print(angka_1)
+
+for i in angka_1:
+    print(f"i sekarang -> {i}")
+
+print("akhir dari program 1\n")
+
+# ini dengan range
+
+angka_2 = range(5)
+
+for i in angka_2:
+    print(f"i sekarang -> {i}")
+
+print("akhir dari program 1\n")
+
+# menggunakan string
+
+data_str = "saya ganteng abiez"
+
+for i in data_str:
+    print(i)
+    
+print("akhir dari program 1\n")
