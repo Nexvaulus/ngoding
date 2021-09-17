45c47dcf3183c75f03a5b9d7d3514ed0363a688b
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-17 08:06:49 +0700

    yes

---
+++ python/percabangan/latihan_percabangan.py
@@ -0,0 +1,29 @@
+# percakapan singkat
+
+
+print(18*'=')
+print("Percakapan Singkat")
+print(18*'=')
+print("")
+
+
+x = 10
+
+while x < 20:
+    x = float(input())
+    print(x)
+
+print(f"Sekarang {x}")
+
+print("")
+
+
+x = "ucup"
+
+while x == "ucup":
+    x = str(input())
+
+print(f"Sekarang {x}")
+
+
+print("Akhir dari program")
--- python/perulangan/while.py
+++ python/perulangan/while.py
@@ -15,3 +15,5 @@ while angka < 5:
     print("Ini adalah izanami")
 
 print("Akhir dari program")
+
+
