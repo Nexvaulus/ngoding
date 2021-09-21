904351c1b5d7c9de1b0dea343e75b412aa41e9b0
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-21 10:49:00 +0700

    yes

--- python/percabangan/latihan_percabangan.py
+++ python/percabangan/latihan_percabangan.py
@@ -1,29 +1,16 @@
-# percakapan singkat
+# latihan
 
 
 print(18*'=')
-print("Percakapan Singkat")
+sekat = "Latihan".center(18)
+print(f"{sekat}")
 print(18*'=')
 print("")
 
 
-x = 10
-
-while x < 20:
-    x = float(input())
-    print(x)
-
-print(f"Sekarang {x}")
-
-print("")
 
 
-x = "ucup"
-
-while x == "ucup":
-    x = str(input())
-
-print(f"Sekarang {x}")
 
 
-print("Akhir dari program")
+
+
