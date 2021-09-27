c8e03c0015c229890f2e8b3528bfc087a609f11c
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-09-27 11:25:00 +0700

    yes

--- javascript/pengkondisian/else_if.js
+++ javascript/pengkondisian/else_if.js
@@ -1,40 +1,39 @@
-var angka = prompt("Masukkan angka ");
-if(angka % 2 === 0) {
-    alert("[" + angka + "] adalah bilangan GENAP");
-}
+// var angka = prompt("Masukkan angka ");
+// if(angka % 2 === 0) {
+//     alert("[" + angka + "] adalah bilangan GENAP");
+// }
 
-else if(angka % 2 === 1) {
-    alert("["  + angka + "] adalah bilangan GANJIL");
-}
+// else if(angka % 2 === 1) {
+//     alert("["  + angka + "] adalah bilangan GANJIL");
+// }
 
-else{
-    alert("Yang anda masukkan bukan angka");
-}
+// else{
+//     alert("Yang anda masukkan bukan angka");
+// }
 
-alert("Akhir dari program")
+// alert("Akhir dari program")
 
 
 console.log("PR : Juragan Angkot [4]");
 var jumlahangkot = 10;
 var angkotberoperasi = 6;
-var angkotlembur = 8;
+var angkotlembur1 = 8;
+var angkotlembur2 = 10;
+var angkotlembur3 = 5;
 var angkot = 1;
 
 for (angkot; angkot <= jumlahangkot; angkot++) {
-	if( angkot <= angkotberoperasi) {
+	if( angkot <= angkotberoperasi && angkot !== angkotlembur3) {
 		console.log('angkot no. ' + angkot + ' beroperasi dengan baik');
-	}
+	} 
 
-	else if(angkot < angkotlembur) {
-        console.log('angkot no. ' + angkot + ' tidak dapat beroperasi.');
-	}
-
-    else if(angkot === angkotlembur) {
+    else if(angkot === angkotlembur1 || angkot === angkotlembur2 || angkot === angkotlembur3) {
         console.log("angkot no. " + angkot + " sedang lembur.");
-    }
+    } 
 
-	else {
+    else {
 		console.log('angkot no. ' + angkot + ' tidak dapat beroperasi.');
 	}
 }
+
 
