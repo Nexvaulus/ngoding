471987bace15d3cefa7a4a4d470a486b368e624b
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-10-04 19:05:01 +0700

    yes

--- javascript/function/pendahuluan.js
+++ javascript/function/pendahuluan.js
@@ -1,23 +1,29 @@
 // nyoba
 
-// function volume2Kubus(x, y) {
-//     x **= 3;
-//     y **= 3;
-//     return (x + y);
-// }
+function volume2Kubus(x, y) {
+    return (x**3 + y**3);
+}
 
-// var x = prompt("Masukkan panjang kubus 1");
-//     y = prompt("Masukkan panjang kubus 2");
+var x = prompt("Masukkan panjang kubus 1");
+    y = prompt("Masukkan panjang kubus 2");
 
-// var kubus = (volume2Kubus(x, y));
-// console.log(kubus);
-// alert("Panjang sisi kubus 1 adalah " + x + "\nPanjang sisi kubus 2 adalah " + y + "\nMaka hasilnya adalah " + kubus);
+var kubus = (volume2Kubus(x, y));
+console.log(kubus);
+alert("Panjang sisi kubus 1 adalah " + x + "\nPanjang sisi kubus 2 adalah " + y + "\nMaka hasilnya adalah " + kubus);
 
-function angkaRandom(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+function coba () {
+  return arguments
+}
+var x = coba(2, 3, "Hello World", true);
+console.log(x);
+
+function tambah() {
+  var hasil = 0;
+  for(var i = 0; i < arguments.length; i++) {
+    hasil += arguments[i];
   }
+  return hasil;
+}
 
-var x = angkaRandom(1, 27)
-console.log(x)
+var coba = tambah(1,2,3);
+console.log(coba)
