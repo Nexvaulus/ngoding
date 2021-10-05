9ef93d34436f54cae390d5943c20bd48c02397e8
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-10-05 19:00:23 +0700

    Yes

---
+++ javascript/array/pendahuluan.js
@@ -0,0 +1,8 @@
+var hari = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"];
+
+console.log(hari[0]);
+console.log(hari.length);
+console.log(typeof(hari));
+
+var myArr = [2, 'string', true, [1,2,3,4]];
+console.log(myArr[3][1])
