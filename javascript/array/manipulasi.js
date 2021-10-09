3033fe521e86989e51ec9a0f39fc07090bad2712
Nexvaulus_Grigs <kaptenmagma2@gmail.com>
2021-10-09 11:02:52 +0700

    Kelar

--- javascript/array/manipulasi.js
+++ javascript/array/manipulasi.js
@@ -21,10 +21,13 @@ for(var i = 0; i < arr.length; i++) {
 
 // method pada array 
 // 1. join
+// join untuk menampilkan isi array dalam bentuk string bukan object
+console.log("");
 var arr = ["Sandhika", "Galih", "Nofa"];
 console.log(arr.join());
 
 // 2. push & pop
+console.log("");
 arr.push("Doddy"); // push
 console.log(arr.join());
 
@@ -33,13 +36,15 @@ arr.pop();
 console.log(arr.join()); // pop
 
 // 3. unshift dan shift
+console.log("");
 arr.unshift("Doddy");
 
 arr.shift();
 console.log(arr.join());
 
 // 4. splice
-// rumus nya splice(indexAwal, mauDihapusberapa, elemenBaru1, elemenBaru2, ...)
+// rumus nya splice(indexAwal, mauDihapusberapa, elemenBaru1, elemenBaru2, ...);
+console.log("");
 var arr = ["Sandhika", "Galih", "Nofa"];
 arr.splice(2, 0, "Doddy");
 console.log(arr.join("-"));
@@ -48,7 +53,64 @@ console.log(arr.join("-"));
 // slice(awal, akhir)
 // misalnya slice(1, 3)
 // yang keambil 1 sama 2
+console.log("");
 var arr = ["Sandhika", "Galih", "Nofa", "Doddy", "Fitri"];
 var arr2 = arr.slice(1, 3);
-console.log(arr2.join())
+console.log(arr2.join());
 
+// 6. forEach
+// forEach adalah for loop tapi lebih sakti karena bisa menggunakan function didalam array
+console.log("");
+var angka = [1,2,3,4,5,6,7,8];
+// for (var  i = 0; i < angka.length; i++) {
+//     console.log(angka[i])
+    // bisa kaya gitu buat munculin array tapi bakal lebih sakti kalo forEach
+// }
+angka.forEach(function(n) {
+    console.log(n);
+});
+
+var nama = ["Sandhika", "Galih", "Nofa"];
+nama.forEach(function(e, i) {
+    console.log("Mahasiswa ke-" + (i+1) + " adalah : "  + e);
+})
+
+// 7.map 
+// map lebih baik dari forEach karena mengembalikan array
+console.log("");
+var angka = [1, 2, 3, 5, 6];
+var angka2 = angka.map(function(r) {
+    return r * 2;
+})
+console.log(angka2.join(","));
+
+// 8. sort
+// sort untuk mengurutkan
+console.log("")
+var angka = [1,2,15,28,39,5,8,4,6];
+console.log(angka.join(" - "))
+angka.sort(function(a,b) {
+    return a - b;
+});
+console.log(angka.join(" - "))
+
+// 9. filter
+// filter mencari nilai dan mengembalikan dalam bentuk array
+console.log("")
+var angka = [1,2,15,28,39,5,8,4,6];
+var angka2 = angka.filter(function(x) {
+    return x > 5;
+})
+angka2.sort(function(a, b) {
+    return a - b;
+});
+console.log(angka2.join(" - "));
+
+// 10. find
+// find hanya mencari satu nilai
+console.log("")
+var angka = [1,2,15,28,39,5,8,4,6];
+var angka2 = angka.find(function(x) {
+    return x == 5;
+})
+console.log(angka2);
