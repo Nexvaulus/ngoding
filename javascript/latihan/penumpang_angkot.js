7278e2586a76db8e4e440e0bd89aaca112e9b639
Nexvaulus <moehammad.fikr@gmail.com>
2021-10-17 09:01:58 +0700

    Menghapus file di dalam array dan menyelesaikan latihan

--- javascript/latihan/penumpang_angkot.js
+++ javascript/latihan/penumpang_angkot.js
@@ -1,1 +1,92 @@
+// latihan 
 
+var penumpang = [];
+// ==== versi panjang ====
+// var tambahPenumpang = function(namaPenumpang, penumpang) {
+//     // jika angkot kosong
+//     if(penumpang.length == 0) {
+//         // tambah penumpang di awal array
+//         penumpang.push(namaPenumpang);
+//         // kembalikan isi array dan keluar dari function
+//         return penumpang;
+//     }    
+//     else {
+//         // telusuri seluruh kursi dari awal
+//         for(var i = 0; i < penumpang.length; i++) {
+//             // jika ada kursi kosong
+//             if(penumpang[i] == undefined) {
+//                 // tambah penumpang di kursi tersebut
+//                 penumpang[i] = namaPenumpang;
+//             }
+//             // jika sudah ada nama yang sama
+//             else if(penumpang[i] == namaPenumpang) {
+//                 // tampilkan pesan kesalahannya
+//                 console.log(namaPenumpang + " sudah ada di dalam angkot.")
+//             }
+//             // jika seluruh kursi terisi
+//             else if(i == penumpang.length - 1) {
+//                 // tambah penumpang di akhir array
+//                 penumpang.push(namaPenumpang);
+//             }
+//     // kembalikan isi array dan keluar dari function
+//     return penumpang;
+//         }
+//     }
+// }
+
+var tambahPenumpang = function(namaPenumpang, penumpang) {
+    for(var i = 0; i < penumpang.length; i++) {
+        if(penumpang[i] == undefined) {
+            return penumpang.splice(i, 1, namaPenumpang);
+        }
+        else if(penumpang[i] == namaPenumpang) {
+            console.log(namaPenumpang + " sudah ada di dalam angkot.");
+            return penumpang;
+        }
+    }
+    return penumpang.push(namaPenumpang);
+}
+
+// ==== versi panjang ====
+// var hapusPenumpang = function(namaPenumpang, penumpang) {
+//     // telusuri seluruh angkot dari awal
+//     for(var i = 0; i < penumpang.length; i++) {
+//     // jika angkot kosong
+//     if(penumpang.length == 0) {
+//         // tampilkan pesan bahwa angkot kosong
+//         console.log("Angkot sedang kosong");
+//         // kembalikan isi array dan keluar dari function
+//         return penumpang;
+//     }
+//     else {
+//             // jika nama penumpang sesuai
+//             if(penumpang[i] == namaPenumpang) {
+//                 // hapus penumpang dengan mengubah menjadi undefined
+//                 penumpang[i] == undefined;
+//                 // kembalikan isi array dan keluar dari function
+//                 return penumpang
+//             }
+//             else if(i == null) {
+//                 console.log(namaPenumpang + " tidak ada di dalam angkot");
+//             }
+//             return penumpang.splice(i, 1, null);
+//         }
+//     }
+// }
+
+var hapusPenumpang = function(namaPenumpang, penumpang) {
+    if(penumpang.length == 0) {
+        console.log("Angkot masih kosong.");
+    }
+    else {
+        for(var i = 0; i < penumpang.length; i++) {
+            if(i == penumpang.length - 1) {
+                console.log(namaPenumpang + " tidak ada di dalam angkot.");
+                return penumpang;
+            }
+            else if(penumpang[i] == namaPenumpang) {
+                return penumpang[i] = undefined;
+            }
+        }
+    }
+} 
