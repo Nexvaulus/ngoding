23d8303c2baa466d6e49c7c87aac9eb6c8740294
Nexvaulus <moehammad.fikr@gmail.com>
2021-10-17 10:41:41 +0700

    Mengedit file pendahuluan object

--- javascript/object/pendahuluan.js
+++ javascript/object/pendahuluan.js
@@ -4,3 +4,15 @@
 // function yang ada di dalam object disebut method
 // contoh
 
+var mahasiswa = {
+    nama : "Ucup",
+    umur : 21,
+    pekerjaan : "mahasiswa",
+    sapa: function() {
+        return "Hi, nama saya " + this.nama + 
+                'usia saya ' + this.umur + " tahun, " + 
+                "dan saya adalah seorang " + this.pekerjaan;
+    }
+}
+
+console.log(mahasiswa.nama)
