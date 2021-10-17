f82e87d52e3514cab584a16b1916f676bb824efc
Nexvaulus <moehammad.fikr@gmail.com>
2021-10-17 09:33:09 +0700

    Menambahkan file intro object

---
+++ javascript/object/pendahuluan.js
@@ -0,0 +1,6 @@
+// object adalah array yang lebih sakti
+// object ditulis dengan kurung kurawal
+// variabel yang ada di dalam object disebut dengan properti
+// function yang ada di dalam object disebut method
+// contoh
+
