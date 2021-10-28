5650af2eb4f473c44de53446e9ee90680dada5f8
Nexvaulus <moehammad.fikr@gmail.com>
2021-10-29 06:54:43 +0700

    Selesai belajar object.create

--- javascript/advance/prototype/object(revisited).js
+++ javascript/advance/prototype/object(revisited).js
@@ -1,6 +1,6 @@
 // cara untuk membuat object pada javascript
 // 1. object literal
-
+// problem : tidak efektif
 let mahasiswa = {
     nama: "Sandhika",
     energi: 10,
@@ -13,22 +13,28 @@ let mahasiswa = {
 
 // 2. function declaration
 
+const methodMahasiswa = {
+    makan : function(porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan`);
+    },
+
+    main : function(jam) {
+        this.energi -= jam;
+        console.log(`Selamat bermain ${this.nama}`);
+    },
+    
+    tidur : function(jam) {
+        this.energi += jam * 2;
+        console.log(`Selamat tidur ${this.nama}`);
+    }
+};
+
 function siswa(nama, umur, energi) {
-    let siswa = {}
+    let siswa = Object.create(methodMahasiswa)
     siswa.nama = nama;
     siswa.umur = umur;
     siswa.energi = energi;
-
-    siswa.makan = function(porsi) {
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan`)
-    }
-
-    siswa.main = function(jam) {
-        this.energi -= jam;
-        console.log(`Selamat bermain ${this.nama}`)
-    }
-
     return siswa;
 }
 
